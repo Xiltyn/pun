081a54729b317d9b0c066c4f09f393085ab6fb2a
Xiltyn <xiltyn@gmail.com>
2018-02-03 13:08:48 +0100

    Testing with electron

---
+++ bin/electron.js
@@ -0,0 +1,38 @@
+// ||  pun  ||
+//   
+// Created @ 12:44 | 2018/02/02  
+// Powered by WebStorm
+//   
+// ------------------------------	
+// |>   From jakubbujko with <3   <|  
+// ------------------------------
+
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+const path = require('path');
+const url = require('url');
+const isDev = require('electron-is-dev');
+
+let mainWindow;
+
+function createWindow() {
+	mainWindow = new BrowserWindow({width: 1280, height: 1024});
+	mainWindow.loadURL( 'http://localhost:3000' );
+	mainWindow.on('closed', () => mainWindow = null);
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
+
+app.on('activate', () => {
+	if (mainWindow === null) {
+		createWindow();
+	}
+});
